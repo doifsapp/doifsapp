3bdf6b2820dad03fb329f8fef6857fa99cfc773d
doifsapp <doifsapp@gmail.com>
2025-03-24 16:23:27 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+#arquivo de configurações básicas
+
+#emal do database > doifaplicacao@gmail.com
+#senha de acesso do emal > doif_sytem7_ifs5
+
+#database
+#usuario > doifaplicacao
+#senha > 65RfPHZHWrZEtf9n
+
---
+++ update.py
@@ -0,0 +1,6 @@
+from app.service.update.update import UpdateDatabase
+year = 0000
+instance = UpdateDatabase(year)
+instance.update()
+
+
